37ee853a7ea51870b1d57989e530a10d7aa05e61
Samyak Jain <139710951+samyakj07@users.noreply.github.com>
2023-08-06 23:59:28 +0530

    Update image(tricolor) using PIL.py

--- image(tricolor) using PIL.py
+++ image(tricolor) using PIL.py
@@ -1,30 +1,22 @@
-# Task 7: NumPy Image Creation Using NumPy, create an image of at least 100 x 100 pixels!
+NumPy Image Creation Using NumPy, create an image of at least 100 x 100 pixels!
 
 
-from PIL import Image, ImageDraw
+import numpy as np
+from PIL import Image
 
+# Create a blank image with white background
+width, height = 100, 100
+image_data = np.ones((height, width, 3), dtype=np.uint8) * 255
 
-def draw_indian_flag():
-    height = 225
-    width = 450
+# Create a red square in the center
+center_x, center_y = width // 2, height // 2
+square_size = 30
+image_data[center_y - square_size // 2:center_y + square_size // 2,
+           center_x - square_size // 2:center_x + square_size // 2] = [255, 0, 0]
 
-    # Create a new image with white background
-    flag = Image.new("RGB", (width, height), (255, 255, 255))
-    draw = ImageDraw.Draw(flag)
+# Create a PIL image from NumPy array
+image = Image.fromarray(image_data)
 
-    # Calculate the heights of different sections in the flag
-    saffron_height = height // 3
-    white_height = height // 3
-    green_height = height // 3
-
-    # Draw the saffron, white, and green sections
-    draw.rectangle([(0, 0), (width, saffron_height)], fill=(255, 153, 51))
-    draw.rectangle([(0, saffron_height), (width, saffron_height + white_height)], fill=(255, 255, 255))
-    draw.rectangle([(0, saffron_height + white_height), (width, height)], fill=(19, 136, 8))
-
-    flag.save("indian_flag.png")
-    flag.show()
-
-
-if __name__ == "__main__":
-    draw_indian_flag()
+# Save the image
+image.save("numpy_image.png")
+print("Image saved as 'numpy_image.png'")
